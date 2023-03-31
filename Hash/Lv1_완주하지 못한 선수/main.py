047035208dfbfacae30f7d5eadeae70abe00809c
HyungJun Jin <55532487+RoBoTics-JHJ@users.noreply.github.com>
2023-03-31 09:22:47 +0900

    Delete Hash directory

--- Hash/Lv1_완주하지 못한 선수/main.py
+++
@@ -1,9 +0,0 @@
-def solution(participant, completion):
-    participant = sorted(participant)
-    completion = sorted(completion)
-
-    for i in range(len(completion)):
-        if participant[i] != completion[i]:
-            return participant[i]
-
-    return participant[-1]
